b521575862d168843005dd47ac5a2440198cc8a6
goooblin4 <goooblin4@mail.ru>
2020-06-09 10:50:48 +0300

    3.3. Разбор первой программы. [#303148]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Andrew Chaban");
+        System.out.println("04.07.2020");
+    }
+}
