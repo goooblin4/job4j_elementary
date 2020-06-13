5cb3d3cb18e1f005aa480c9c2161d515f4a6db20
goooblin4 <goooblin4@mail.ru>
2020-06-13 16:34:51 +0300

    9.1. String [#303138]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        String idea2 = "But I am a newbie.";
+        String phrase = idea + " " + idea2;
+        System.out.println(phrase);
+        int year = 2020;
+        String full = phrase + " " + year;
+        System.out.println(full);
+
+    }
+}
