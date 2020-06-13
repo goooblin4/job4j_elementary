6f2a9e9f6e3c83e51bb66a0def76a2dc0ac0892a
goooblin4 <goooblin4@mail.ru>
2020-06-13 16:55:28 +0300

    9.1. String [#303138]

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -5,14 +5,16 @@ public class SqArea {
         double h = p / (2 * (k + 1));
         return h;
     }
+
     public static double square2(int p, int k) {
         double h = p / (2 * (k + 1));
         double L = h * k;
         return L;
     }
+
     public static void main(String[] args) {
         double result1 = SqArea.square(6, 2);
-        double result2 = SqArea.square2(6,2);
+        double result2 = SqArea.square2(6, 2);
         System.out.println(" p = 6, k = 2, s = 2, h = " + result1 + " L = " + result2);
     }
 }
