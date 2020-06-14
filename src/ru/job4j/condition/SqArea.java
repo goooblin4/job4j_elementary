3ffbd50c39fdd9cb6deb483eac95c226c5e98e74
goooblin4 <goooblin4@mail.ru>
2020-06-14 13:53:10 +0300

    9.2. Checkstyle [#303143]

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -8,13 +8,13 @@ public class SqArea {
 
     public static double square2(int p, int k) {
         double h = p / (2 * (k + 1));
-        double L = h * k;
-        return L;
+        double l = h * k;
+        return l;
     }
 
     public static void main(String[] args) {
         double result1 = SqArea.square(6, 2);
         double result2 = SqArea.square2(6, 2);
-        System.out.println(" p = 6, k = 2, s = 2, h = " + result1 + " L = " + result2);
+        System.out.println(" p = 6, k = 2, h = " + result1 + " L = " + result2);
     }
 }
